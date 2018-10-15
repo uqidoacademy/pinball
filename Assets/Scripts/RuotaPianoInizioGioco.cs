dc46057eecfef25f7f4ead5825e3388f5f9f3e62
Riccardo Giorato <giorat.96@gmail.com>
2018-10-15 16:42:07 +0200

    aggiunta blockout e pushawayball

---
+++ Assets/Scripts/RuotaPianoInizioGioco.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuotaPianoInizioGioco : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// ruoto il piano rendendolo inclinato
+		this.transform.eulerAngles = new Vector3(-80f,0,0);
+	}
+
+}
---
+++ Assets/Scripts/pushAwayBall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pushAwayBall : MonoBehaviour {
+
+    public void OnCollisionEnter (Collision pallina) {
+        
+        if (pallina.collider.tag == ("pallina")) {
+            var force = pallina.transform.position - transform.position;
+            force.Normalize ();
+            pallina.gameObject.GetComponent<Rigidbody> ().AddForce (force * Random.Range (800, 1500));
+            //TODO aggiungo segnalazione AGGIUNTA punteggio
+        }//if pallina
+
+    } //OnCollisionEnter
+
+
+} //pushAwayBall
