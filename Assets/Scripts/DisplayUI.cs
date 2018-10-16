d1c9546222d1472317819bf16cf532047a5dbc5a
Unknown <alessioliuzzi98@gmail.com>
2018-10-16 16:04:24 +0200

    Aggiunto multiball e UI

---
+++ Assets/Scripts/DisplayUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayUI : MonoBehaviour {
+
+    public int score;
+    public int lives;
+    public bool GameOver = false;
+
+    void OnGUI()
+    {
+        if (GameOver)
+        {
+            GUI.Label(new Rect(20, 20, Screen.width, Screen.height), "<size=30>Game over! Press R to Restart</size>");
+        }
+        GUI.contentColor = Color.red;
+        GUI.Label(new Rect(20, 60, Screen.width, Screen.height),  "<size=30>Lives  = </size>" + "<size=30>" + lives + "</size>");
+        GUI.Label(new Rect(20, 100, Screen.width, Screen.height), "<size=30>Score = </size>" + "<size=30>" + score + "</size>");
+
+
+    }
+}
---
+++ Assets/Scripts/MultiBall.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiBall : MonoBehaviour {
+
+    private GameObject newBall;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        newBall = Instantiate(collision.gameObject, collision.gameObject.transform.position, collision.gameObject.transform.rotation);
+    }
+
+}
