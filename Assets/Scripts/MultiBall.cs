ce14a34374bf44360e322ffcfee678017d578607
Unknown <alessioliuzzi98@gmail.com>
2018-10-16 17:52:48 +0200

    Fix bug multiball

--- Assets/Scripts/MultiBall.cs
+++ Assets/Scripts/MultiBall.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MultiBall : MonoBehaviour {
-
+    private bool readyToSpawn = true;
     private GameObject newBall;
 
     private void OnCollisionEnter (Collision collision) {
-        ContatorePalline.aggiungiPalline ();
-        newBall = Instantiate (collision.gameObject, collision.gameObject.transform.position, collision.gameObject.transform.rotation);
+        if (readyToSpawn)
+        {
+            ContatorePalline.aggiungiPalline();
+            newBall = Instantiate(collision.gameObject, collision.gameObject.transform.position, collision.gameObject.transform.rotation);
+            StartCoroutine(WaitForSpawn());
+        }
     }
-
+    IEnumerator WaitForSpawn()
+    {
+        readyToSpawn = false;
+        yield return new WaitForSeconds(1);
+        readyToSpawn = true;
+    }
 }
