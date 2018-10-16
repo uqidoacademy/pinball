b96134a95812278554c4ee540cbbe400f4954b70
Riccardo Giorato <giorat.96@gmail.com>
2018-10-16 16:56:26 +0200

    COMMIT FINALE -> gioco da vendere

--- Assets/Scripts/PushKeyThrowBall.cs
+++ Assets/Scripts/PushKeyThrowBall.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PushKeyThrowBall : MonoBehaviour {
-  
+
     public int minSpeed;
     public KeyCode keyCode;
     public int maxSpeed;
@@ -13,69 +13,41 @@ public class PushKeyThrowBall : MonoBehaviour {
     private float intensity;
     private bool isCollision;
     Rigidbody rigidBody;
-    
-
 
     // Use this for initialization
     void Start () {
-
         isCollision = false;
         intensity = 0;
-        
-     
-
     }
-	
-    private void OnCollisionEnter(Collision collision)
-    {
-        
+
+    private void OnCollisionEnter (Collision collision) {
+
         isCollision = true;
-        Debug.Log("enter");
-        speed = Random.Range(minSpeed, maxSpeed);
-      
-        rigidBody = collision.gameObject.GetComponent<Rigidbody>();
+        Debug.Log ("enter");
+        speed = Random.Range (minSpeed, maxSpeed);
+
+        rigidBody = collision.gameObject.GetComponent<Rigidbody> ();
         ball = collision.gameObject;
     }
-    
 
-    private void OnCollisionExit(Collision collision)
-    {
-        
+    private void OnCollisionExit (Collision collision) {
         isCollision = false;
-        Debug.Log("exit");
+        Debug.Log ("exit");
     }
-    private void Update()
-    {
-        if (isCollision)
-        {
-            //Debug.Log("quadro");
-            if (Input.GetKeyUp(keyCode))
-            {
-                rigidBody.AddForce(new Vector3(ball.transform.position.x, ball.transform.position.y, -ball.transform.position.z * speed * intensity), ForceMode.Force);
-                Debug.Log("uao");
+    private void Update () {
+        if (isCollision) {
+            if (Input.GetKeyUp (keyCode)) {
+                rigidBody.AddForce (new Vector3 (ball.transform.position.x, ball.transform.position.y, -ball.transform.position.z * speed * intensity), ForceMode.Force);
+                Debug.Log ("uao");
             }
         }
-     
-            
-        if (Input.GetKey(keyCode))
-        {
+
+        if (Input.GetKey (keyCode)) {
             intensity += Time.deltaTime;
-            //Debug.Log(intensity);
-        }
-        else
-        {
+        } else {
             intensity = 0;
         }
 
-
-
-
-       
-        
-        
-
-        
     }
-
 
 }
---
+++ Assets/blackHolePull.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blackHolePull : MonoBehaviour {
+
+	void OnTriggerEnter (Collider collider) {
+		collider.GetComponent<Rigidbody> ().isKinematic = true;
+		StartCoroutine(AnimateMove( collider.transform.localPosition, gameObject.transform.localPosition, 1f,collider.transform));
+		collider.GetComponent<Rigidbody> ().isKinematic = false;
+	}
+
+	IEnumerator AnimateMove (Vector3 origin, Vector3 target, float duration,Transform objectToMove) {
+		float journey = 0f;
+		while (journey <= duration) {
+			journey = journey + Time.deltaTime;
+			float percent = Mathf.Clamp01 (journey / duration);
+
+			objectToMove.transform.position = Vector3.Lerp (origin, target, percent);
+
+			yield return null;
+		}
+	}
+}
